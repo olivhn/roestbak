007bd637e0d531a864b318bc559d74264afd13f5
Olivier Verhoogen <olivier.verhoogen@gmail.com>
2023-03-24 10:42:09 +0100

    Set up logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use crate::logging::SimpleLogger;
+use std::process;
+
+mod logging;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    SimpleLogger::install()?;
+
+    log::info!("Starting roestbak service with PID {}.", process::id());
+
+    Ok(())
 }
